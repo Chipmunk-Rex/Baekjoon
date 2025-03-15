28b4b0e2fbf946ef5794f8d5b7933e995a8108b1
rex렉스 <157000395+Chipmunk-Rex@users.noreply.github.com>
2025-03-15 23:38:14 +0900

    [Platinum V] Title: 약 팔기, Time: 172 ms, Memory: 6628 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cs
@@ -0,0 +1,21 @@
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        n = 1000000;
+        int t = 0;
+        Console.WriteLine(2000);
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.Write(1);
+            Console.Write(' ');
+        }
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.Write(1000);
+            Console.Write(' ');
+        }
+    }
+}
