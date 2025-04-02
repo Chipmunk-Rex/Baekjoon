0568c63a328b23135259beeb5047bb5b7eef1537
rex렉스 <157000395+Chipmunk-Rex@users.noreply.github.com>
2025-04-02 16:34:25 +0900

    [Bronze V] Title: Hook, Time: 44 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cs
@@ -0,0 +1,13 @@
+
+using System.Numerics;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(@"#  # #### #### #  #
+#### #  # #  # # #
+#### #  # #  # # #
+#  # #### #### #  #");
+    }
+}
