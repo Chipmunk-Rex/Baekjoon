700bc0a04d6e849452cf1476fe97eca340fb64ba
rex렉스 <157000395+Chipmunk-Rex@users.noreply.github.com>
2025-04-27 12:56:42 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 1164 ms, Memory: 6580 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.cs
@@ -0,0 +1,24 @@
+
+
+    using System.Numerics;
+using System.Runtime.ExceptionServices;
+
+internal class Program
+{
+     static void Main(string[] args)
+     {
+         int n = int.Parse(Console.ReadLine());
+         int max = n * 2 - 1;
+        for (int y = 0; y < max; y++)
+        {
+            for (int x = 0; x <= max; x++)
+            {
+                if (x <= y - ((y % n + 1) * (y / n) * 2) || x > max - y - 1 + ((y % n + 1) * (y / n) * 2))
+                     Console.Write("*");
+                 else
+                     Console.Write(" ");
+             }
+             Console.WriteLine();
+          }
+     }   
+}
