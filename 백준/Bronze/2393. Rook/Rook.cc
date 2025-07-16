f86dca08274d900080c8b59f1c53d2f56cafcc7a
rex렉스 <157000395+Chipmunk-Rex@users.noreply.github.com>
2025-07-16 09:35:56 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+cout << ("  ___  ___  ___");
+cout << endl << ("  | |__| |__| |");
+cout << endl << ("  |           |");
+cout << endl << ("   \\_________/");
+cout << endl << ("    \\_______/");
+cout << endl << ("     |     |");
+cout << endl << ("     |     |");
+cout << endl << ("     |     |");
+cout << endl << ("     |     |");
+cout << endl << ("     |_____|");
+cout << endl << ("  __/       \\__");
+cout << endl << (" /             \\");
+cout << endl << ("/_______________\\");
+}
