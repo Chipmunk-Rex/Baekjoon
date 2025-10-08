e0f32e933ef939a78af92f01b84ea5a51d00adb8
rex렉스 <157000395+Chipmunk-Rex@users.noreply.github.com>
2025-10-08 17:01:29 +0900

    [Bronze V] Title: 사칙연산, Time: 52 ms, Memory: 5508 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(' ');
+            Console.WriteLine(int.Parse(str[0]) + int.Parse(str[1]));
+            Console.WriteLine(int.Parse(str[0]) - int.Parse(str[1]));
+            Console.WriteLine(int.Parse(str[0]) * int.Parse(str[1]));
+            Console.WriteLine(int.Parse(str[0]) / int.Parse(str[1]));
+            Console.WriteLine(int.Parse(str[0]) % int.Parse(str[1]));
+        }
+    }
+}
