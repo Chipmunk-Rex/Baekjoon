9fd39155cc82145937eb6e3a78fc8fc11b4d4bed
rex렉스 <157000395+Chipmunk-Rex@users.noreply.github.com>
2026-03-09 11:52:01 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<string>
+#include <queue>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cout << "A";
+}
