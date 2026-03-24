9529a8ce90671ae6a76174b659f7351382f6dc9d
rex렉스 <157000395+Chipmunk-Rex@users.noreply.github.com>
2026-03-24 09:33:01 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<string>
+#include <queue>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <cmath>
+
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	if (n % 4 == 0 && (n % 100 != 0 || n % 400 == 0))
+		cout << 1;
+	else cout << 0;
+}
