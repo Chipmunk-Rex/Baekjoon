1fc41dd92b9d04c7e904777ff61382c0cebd55ce
rex렉스 <157000395+Chipmunk-Rex@users.noreply.github.com>
2026-03-24 10:37:47 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<string>
+#include <queue>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cout << "         ,r'\"7\n";
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |\n";
+}
