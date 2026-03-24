a65b5c54810ab5ff9a5b8e180cae9674020abd55
rex렉스 <157000395+Chipmunk-Rex@users.noreply.github.com>
2026-03-24 11:45:42 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<string>
+#include <queue>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (i + j < n-1)
+				cout << ' ';
+			else
+				cout << '*';
+		}
+		cout << '\n';
+	}
+}
