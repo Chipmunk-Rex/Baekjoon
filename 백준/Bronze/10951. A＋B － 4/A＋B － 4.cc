6def4bc8eedd731fe1203f92f6f04c59e4407148
rex렉스 <157000395+Chipmunk-Rex@users.noreply.github.com>
2026-03-24 09:54:47 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<string>
+#include <queue>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int a, b;
+	while (cin >> a >> b)
+	{
+		cout << a + b << '\n';
+	}
+
+}
